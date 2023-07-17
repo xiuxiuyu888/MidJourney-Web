cc036c9bebd3e17c99f1c2daecd56c0efc6e59ca
xiuxiuyu888 <3969347@qq.com>
2023-07-18 04:03:50 +0800

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,16 +11,16 @@ const Header = () => {
             </div>
             <div className="fi mt-2">
                 <span className="gpt-title">MIDJOURNEY</span>
-                <span className="gpt-subtitle">WEB</span>
+                <span className="gpt-subtitle">Beta1(测试版)</span>
             </div>
             <p className="mt-1 opacity-60 ">
                 Powered by
                 <a
                     className="b-buy-link ml-1 decoration-wavy underline-offset-8"
-                    href={ GITHUB_ORG_URL } target="_blank"
+                    href="http://ww.aahezu.com" target="_blank"
                     rel="noopener noreferrer"
                 >
-                     ConnectAI-E
+                     AAhezu
                 </a>
             </p>
         </header>
